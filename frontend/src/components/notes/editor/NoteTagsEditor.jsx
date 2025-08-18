61816ab9d47bf413e4d13081a1562ec408965f61
Johnny Jachna <johnnyjachna@gmail.com>
2025-08-18 19:21:02 -0400

    improve note tags and move to own component

--- frontend/src/components/notes/editor/NoteTagsEditor.jsx
+++ frontend/src/components/notes/editor/NoteTagsEditor.jsx
@@ -1,8 +1,16 @@
 import { useState, useEffect, useMemo } from "react";
 import { tagsAtom } from "../../tags/TagsAtoms";
 import { useAtomValue } from "jotai/react";
+import NoteTag from "./NoteTag";
 
-import { Badge, Button, Dropdown, DropdownItem } from "flowbite-react";
+import {
+  Badge,
+  Button,
+  Dropdown,
+  DropdownItem,
+  Toast,
+  ToastToggle,
+} from "flowbite-react";
 
 const NoteTagsEditor = ({ tags, setTags }) => {
   const allTags = useAtomValue(tagsAtom);
@@ -10,10 +18,10 @@ const NoteTagsEditor = ({ tags, setTags }) => {
   const addableTags = useMemo(
     () =>
       allTags.filter((tag) => !tags.some((noteTag) => noteTag.id === tag.id)),
-    [allTags, tags],
+    [allTags, tags]
   );
   const [tagSelection, setTagSelection] = useState(
-    addableTags.length > 0 ? addableTags[0].id : "",
+    addableTags.length > 0 ? addableTags[0].id : ""
   );
   useEffect(() => {
     if (addableTags.length > 0) setTagSelection(addableTags[0]);
@@ -34,16 +42,11 @@ const NoteTagsEditor = ({ tags, setTags }) => {
       <ul className="flex flex-row flex-wrap gap-2">
         {tags.map((tag) => (
           <>
-            <Badge key={tag.id + tag.name} size="sm" color="yellow">
-              {tag.name}
-            </Badge>
-            <Button
-              onClick={() => handleRemoveTag(tag.id)}
-              size="xs"
-              color="alternative"
-            >
-              x
-            </Button>
+            <NoteTag
+              key={tag.id + tag.name}
+              tag={tag}
+              handleRemoveTag={handleRemoveTag}
+            />
           </>
         ))}
       </ul>
