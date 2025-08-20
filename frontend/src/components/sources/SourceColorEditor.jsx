23ed145bafd9c20a4e204c9d864f17140e742a8e
Johnny Jachna <johnnyjachna@gmail.com>
2025-08-20 17:19:11 -0400

    add color picker to source editor

---
+++ frontend/src/components/sources/SourceColorEditor.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+import ColorPicker from "@/components/ColorPicker";
+
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "flowbite-react";
+
+const SourceColorEditor = ({
+  color,
+  setColor,
+  open,
+  handleCloseColorEditor,
+}) => {
+  const [newColor, setNewColor] = useState(color);
+
+  const handleCancel = () => {
+    setColor(color);
+    handleCloseColorEditor();
+  };
+
+  const handleSave = () => {
+    setColor(newColor);
+    handleCloseColorEditor();
+  };
+
+  return (
+    <Modal
+      dismissible
+      show={open}
+      onClose={handleCancel}
+      size="sm"
+      className="text-gray-100"
+    >
+      <ModalHeader>Source Color Editor</ModalHeader>
+      <ModalBody className="flex justify-center">
+        <ColorPicker value={newColor} onChange={setNewColor} />
+      </ModalBody>
+      <ModalFooter className="flex justify-center ">
+        <Button onClick={handleSave} color="green">
+          Save
+        </Button>
+      </ModalFooter>
+    </Modal>
+  );
+};
+
+export default SourceColorEditor;
--- frontend/src/components/sources/SourceEditor.jsx
+++ frontend/src/components/sources/SourceEditor.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
+
 import { useAtomValue, useSetAtom } from "jotai/react";
 import { updateSourceAtom } from "./SourcesAtoms";
+
+import ButtonColorToggle from "../buttons/ButtonColorToggle";
+import SourceColorEditor from "./SourceColorEditor";
 
 import {
   Button,
@@ -10,6 +14,7 @@ import {
   ModalHeader,
   TextInput,
 } from "flowbite-react";
+import Source from "./Source";
 
 const SourceEditor = ({ sourceAtom, open, handleCloseEditor }) => {
   const source = useAtomValue(sourceAtom);
@@ -24,6 +29,12 @@ const SourceEditor = ({ sourceAtom, open, handleCloseEditor }) => {
   const [publishDate, setPublishDate] = useState(source.publish_date);
   const [updateDate, setUpdateDate] = useState(source.update_date);
   const [accessDate, setAccessDate] = useState(source.access_date);
+
+  const [colorEditorOpen, setColorEditorOpen] = useState(false);
+
+  const handleCloseColorEditor = () => {
+    setColorEditorOpen(false);
+  };
 
   const handleCancel = () => {
     setName(source.name);
@@ -56,108 +67,121 @@ const SourceEditor = ({ sourceAtom, open, handleCloseEditor }) => {
   };
 
   return (
-    <Modal
-      dismissible
-      show={open}
-      onClose={handleCancel}
-      size="lg"
-      className="text-gray-100"
-    >
-      <ModalHeader>Edit Source</ModalHeader>
-      <ModalBody className="flex flex-col gap-2">
-        <div className="flex items-center gap-2">
-          <h4 className="w-32 text-sm font-medium">Name:</h4>
-          <TextInput
-            id="Name"
-            placeholder="Enter Name..."
-            className="flex-1"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </div>
-        <div className="flex items-center gap-2">
-          <h4 className="w-32 text-sm font-medium">Title:</h4>
-          <TextInput
-            id="Title"
-            placeholder="Enter Title..."
-            className="flex-1"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </div>
+    <>
+      <Modal
+        dismissible
+        show={open}
+        onClose={handleCancel}
+        size="lg"
+        className="text-gray-100"
+      >
+        <ModalHeader>
+          <div className="flex items-center gap-2">
+            <p>Edit Source</p>
+            <ButtonColorToggle onClick={setColorEditorOpen} />
+          </div>
+        </ModalHeader>
+        <ModalBody className="flex flex-col gap-2">
+          <div className="flex items-center gap-2">
+            <h4 className="w-32 text-sm font-medium">Name:</h4>
+            <TextInput
+              id="Name"
+              placeholder="Enter Name..."
+              className="flex-1"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className="flex items-center gap-2">
+            <h4 className="w-32 text-sm font-medium">Title:</h4>
+            <TextInput
+              id="Title"
+              placeholder="Enter Title..."
+              className="flex-1"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
 
-        <div className="flex items-center gap-2">
-          <h4 className="w-32 text-sm font-medium">Author(s):</h4>
-          <TextInput
-            id="Author"
-            placeholder="Enter Author(s)..."
-            className="flex-1"
-            value={authors}
-            onChange={(e) => setAuthors(e.target.value)}
-          />
-        </div>
+          <div className="flex items-center gap-2">
+            <h4 className="w-32 text-sm font-medium">Author(s):</h4>
+            <TextInput
+              id="Author"
+              placeholder="Enter Author(s)..."
+              className="flex-1"
+              value={authors}
+              onChange={(e) => setAuthors(e.target.value)}
+            />
+          </div>
 
-        <div className="flex items-center gap-2">
-          <h4 className="w-32 text-sm font-medium">Publisher(s):</h4>
-          <TextInput
-            id="Publisher"
-            placeholder="Enter Publisher(s)..."
-            className="flex-1"
-            value={publishers}
-            onChange={(e) => setPublishers(e.target.value)}
-          />
-        </div>
+          <div className="flex items-center gap-2">
+            <h4 className="w-32 text-sm font-medium">Publisher(s):</h4>
+            <TextInput
+              id="Publisher"
+              placeholder="Enter Publisher(s)..."
+              className="flex-1"
+              value={publishers}
+              onChange={(e) => setPublishers(e.target.value)}
+            />
+          </div>
 
-        <div className="flex items-center gap-2">
-          <h4 className="w-32 text-sm font-medium">Page(s):</h4>
-          <TextInput
-            id="Pages"
-            placeholder="Enter Page(s)..."
-            className="flex-1"
-            value={pages}
-            onChange={(e) => setPages(e.target.value)}
-          />
-        </div>
+          <div className="flex items-center gap-2">
+            <h4 className="w-32 text-sm font-medium">Page(s):</h4>
+            <TextInput
+              id="Pages"
+              placeholder="Enter Page(s)..."
+              className="flex-1"
+              value={pages}
+              onChange={(e) => setPages(e.target.value)}
+            />
+          </div>
 
-        <div className="flex items-center gap-2">
-          <h4 className="w-32 text-sm font-medium">Date Published:</h4>
-          <TextInput
-            id="PublishDate"
-            placeholder="Date Published"
-            className="flex-1"
-            value={publishDate}
-            onChange={(e) => setPublishDate(e.target.value)}
-          />
-        </div>
+          <div className="flex items-center gap-2">
+            <h4 className="w-32 text-sm font-medium">Date Published:</h4>
+            <TextInput
+              id="PublishDate"
+              placeholder="Date Published"
+              className="flex-1"
+              value={publishDate}
+              onChange={(e) => setPublishDate(e.target.value)}
+            />
+          </div>
 
-        <div className="flex items-center gap-2">
-          <h4 className="w-32 text-sm font-medium">Date Updated:</h4>
-          <TextInput
-            id="UpdateDate"
-            placeholder="Date Updated"
-            className="flex-1"
-            value={updateDate}
-            onChange={(e) => setUpdateDate(e.target.value)}
-          />
-        </div>
+          <div className="flex items-center gap-2">
+            <h4 className="w-32 text-sm font-medium">Date Updated:</h4>
+            <TextInput
+              id="UpdateDate"
+              placeholder="Date Updated"
+              className="flex-1"
+              value={updateDate}
+              onChange={(e) => setUpdateDate(e.target.value)}
+            />
+          </div>
 
-        <div className="flex items-center gap-2">
-          <h4 className="w-32 text-sm font-medium">Date Accessed:</h4>
-          <TextInput
-            id="AccessDate"
-            placeholder="Date Accessed"
-            className="flex-1"
-            value={accessDate}
-            onChange={(e) => setAccessDate(e.target.value)}
-          />
-        </div>
-      </ModalBody>
-      <ModalFooter className="justify-center">
-        <Button onClick={handleSave} color="green">
-          Save
-        </Button>
-      </ModalFooter>
-    </Modal>
+          <div className="flex items-center gap-2">
+            <h4 className="w-32 text-sm font-medium">Date Accessed:</h4>
+            <TextInput
+              id="AccessDate"
+              placeholder="Date Accessed"
+              className="flex-1"
+              value={accessDate}
+              onChange={(e) => setAccessDate(e.target.value)}
+            />
+          </div>
+        </ModalBody>
+        <ModalFooter className="justify-center">
+          <Button onClick={handleSave} color="green">
+            Save
+          </Button>
+        </ModalFooter>
+      </Modal>
+      <SourceColorEditor
+        color={color}
+        setColor={setColor}
+        open={colorEditorOpen}
+        handleCloseColorEditor={handleCloseColorEditor}
+      />
+    </>
   );
 };
 
