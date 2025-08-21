cd421491716ee78ad5dba10ddc9be0cafac3d9f5
Johnny Jachna <johnnyjachna@gmail.com>
2025-08-20 22:05:54 -0400

    add set update on open, sets now ordered by most recently opened

--- frontend/src/components/sets/Set.jsx
+++ frontend/src/components/sets/Set.jsx
@@ -2,19 +2,21 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router";
 
 import { useAtom, useSetAtom } from "jotai/react";
-import { deleteSetAtom } from "./SetsAtoms";
+import { updateSetAtom, deleteSetAtom } from "./SetsAtoms";
 
 import SetEditor from "./SetEditor";
 
-import { Card } from "flowbite-react";
+import { Card, Spinner } from "flowbite-react";
 import ButtonEdit from "../buttons/ButtonEdit";
 import ButtonDelete from "../buttons/ButtonDelete";
 
 const Set = ({ setAtom }) => {
   const [set, setSet] = useAtom(setAtom);
+  const updateSet = useSetAtom(updateSetAtom);
   const deleteSet = useSetAtom(deleteSetAtom);
 
   const [showEditor, setShowEditor] = useState(false);
+  const [opening, setOpening] = useState(false);
   const navigate = useNavigate();
 
   const handleCloseEditor = () => {
@@ -25,8 +27,12 @@ const Set = ({ setAtom }) => {
     await deleteSet(set.id);
   };
 
-  const navToSet = () => {
-    if (!showEditor) navigate(`/sets/${set.id}`);
+  const navToSet = async () => {
+    if (!showEditor) {
+      setOpening(true);
+      await updateSet(set);
+      navigate(`/sets/${set.id}`);
+    }
   };
 
   return (
@@ -41,35 +47,49 @@ const Set = ({ setAtom }) => {
         }
       }}
     >
-      {!showEditor && (
-        <div
-          className="absolute top-2 right-2 flex gap-1 opacity-0 group-hover:opacity-100 transition-opacity"
-          onClick={(e) => e.stopPropagation()}
-          onKeyDown={(e) => e.stopPropagation()}
-        >
-          <ButtonEdit onClick={() => setShowEditor(true)} />
-          <ButtonDelete onClick={handleDeleteSet} />
-        </div>
-      )}
+      {" "}
+      {!opening ? (
+        <>
+          {!showEditor && (
+            <div
+              className="absolute top-2 right-2 flex gap-1 opacity-0 group-hover:opacity-100 transition-opacity"
+              onClick={(e) => e.stopPropagation()}
+              onKeyDown={(e) => e.stopPropagation()}
+            >
+              <ButtonEdit onClick={() => setShowEditor(true)} />
+              <ButtonDelete onClick={handleDeleteSet} />
+            </div>
+          )}
 
-      {showEditor ? (
-        <div
-          className="flex items-center h-10"
-          onClick={(e) => e.stopPropagation()}
-          onKeyDown={(e) => e.stopPropagation()}
-        >
-          <SetEditor
-            setAtom={setAtom}
-            setSet={setSet}
-            handleCloseEditor={handleCloseEditor}
-          />
-        </div>
+          {showEditor ? (
+            <div
+              className="flex items-center h-10"
+              onClick={(e) => e.stopPropagation()}
+              onKeyDown={(e) => e.stopPropagation()}
+            >
+              <SetEditor
+                setAtom={setAtom}
+                setSet={setSet}
+                handleCloseEditor={handleCloseEditor}
+              />
+            </div>
+          ) : (
+            <div className="flex items-center h-10">
+              <h5
+                className="font-semibold truncate pr-16 w-full"
+                title={set.name}
+              >
+                {set.name}
+              </h5>
+            </div>
+          )}
+        </>
       ) : (
-        <div className="flex items-center h-10">
-          <h5 className="font-semibold truncate pr-16 w-full" title={set.name}>
-            {set.name}
-          </h5>
-        </div>
+        <>
+          <div className="flex items-center justify-center">
+            <Spinner />
+          </div>
+        </>
       )}
     </Card>
   );
