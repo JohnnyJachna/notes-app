84f7c51b4709af77f0b5e0f24319943878c7b2f1
Johnny Jachna <johnnyjachna@gmail.com>
2025-08-22 03:17:00 -0400

    add max width

--- frontend/src/components/sets/SetsSection.jsx
+++ frontend/src/components/sets/SetsSection.jsx
@@ -24,11 +24,13 @@ const SetsSection = () => {
   };
 
   return (
-    <div className="m-3 grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
-      {setsList.map((setAtom) => (
-        <Set setAtom={setAtom} key={setAtom.toString()} />
-      ))}
-      <SetAdd handleAddSet={handleAddSet} />
+    <div className="max-w-8/12 mx-auto">
+      <div className="m-3 grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5">
+        {setsList.map((setAtom) => (
+          <Set setAtom={setAtom} key={setAtom.toString()} />
+        ))}
+        <SetAdd handleAddSet={handleAddSet} />
+      </div>
     </div>
   );
 };
